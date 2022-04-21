c96ae0d527ed814becfff13e940f07ec6c8540e1
nathuchoa <nathaliauchoa@hotmail.com>
2022-04-21 14:48:36 -0300

    JS da Ninfadora está funcionando corretamente, botões funcionando corretamente.

--- pages/ninfadora/ninfadora.js
+++ pages/ninfadora/ninfadora.js
@@ -8,17 +8,13 @@ function validarRespostaNinfadoraFase1(respostaCerta) {
       alert("Nossa aventura continua!");
       location.assign("ninfadora-fase2.html");
       break;
-    } else if (resposta != respostaCerta) {
-      if (resposta === "1");
-      {
-        alert("Bagman não teria apostado...");
-        location.assign("game-over-ninfadora.html");
-        break;
-      }
-    } else if (resposta === "3");
-    {
+    } else if (resposta === "1") {
+      alert("Bagman não teria apostado...");
+      location.assign("game-over-ninfadora.html");
+      break;
+    } else (resposta === "3");{
       alert("Hmmm... não estou sei se seguimos pelo caminho correto...");
-      location.assign("ninfadora-fase3.html");
+      location.assign("ninfadora-fase-extra.html");
       break;
     }
   }
@@ -28,22 +24,19 @@ function validarRespostaNinfadoraFase1(respostaCerta) {
 
 function validarRespostaNinfadoraFase2(respostaCerta) {
   while (true) {
+
     var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
 
     if (resposta === respostaCerta) {
       alert("Nossa aventura continua!");
       location.assign("ninfadora-fase3.html");
       break;
-    } else if (resposta != respostaCerta) {
-      if (resposta === "1");
-      {
+    } else if (resposta === "1") {
         alert("Bagman não teria apostado...");
         location.assign("game-over-ninfadora.html");
         break;
-      }
-    } else if (resposta === "3");
-    {
-      alert("Hmmm... não estou sei se seguimos pelo caminho correto...");
+    } else (resposta === "2");{
+      alert("Hmmm... não deve ser isso. Tente outra vez!");
       location.assign("ninfadora-fase2.html");
       break;
     }
@@ -52,52 +45,45 @@ function validarRespostaNinfadoraFase2(respostaCerta) {
 
 //FASE 3 - FINAL//
 
-function validarRespostaNinfadoraFaseExtra(respostaCerta) {
+function validarRespostaNinfadoraFase3(respostaCerta) {
   while (true) {
+
     var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
 
     if (resposta === respostaCerta) {
       alert("Nossa aventura continua!");
-      location.assign("ninfadora-fase2.html");
+      location.assign("../final-vencedor.html");
       break;
-    } else if (resposta != respostaCerta) {
-      if (resposta === "1");
-      {
+    } else if (resposta === "3"){
         alert("Bagman não teria apostado...");
         location.assign("game-over-ninfadora.html");
         break;
-      }
-    } else if (resposta === "3");
-    {
-      alert("Hmmm... não estou sei se seguimos pelo caminho correto...");
+    } else(resposta === "1");{
+      alert("Pelas barbas de Merlim, isso seria muita loucura! Tente outra vez.");
       location.assign("ninfadora-fase3.html");
       break;
     }
   }
 }
 
-
 //FASE - EXTRA - CAMINHO LONGO//
 
 function validarRespostaNinfadoraFaseExtra(respostaCerta) {
   while (true) {
+
     var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
 
     if (resposta === respostaCerta) {
       alert("Nossa aventura continua!");
       location.assign("ninfadora-fase2.html");
       break;
-    } else if (resposta != respostaCerta) {
-      if (resposta === "1");
-      {
-        alert("Bagman não teria apostado...");
+    } else if (resposta === "2"){
+        alert("Definitivamente não estamos no caminho certo...");
         location.assign("game-over-ninfadora.html");
         break;
-      }
-    } else if (resposta === "3");
-    {
-      alert("Hmmm... não estou sei se seguimos pelo caminho correto...");
-      location.assign("ninfadora-fase3.html");
+    } else if (resposta === "3");{
+      alert("Hmmm... esse ainda não é o caminho correto. Melhor tentar de novo.");
+      location.assign("ninfadora-fase1.html");
       break;
     }
   }
