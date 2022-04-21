643839f3c478026ebaef96478523a18ec20144ed
nathuchoa <nathaliauchoa@hotmail.com>
2022-04-21 01:05:01 -0300

    organização de textos e testes do js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-function fase1Cedric() {
-  var contador = 0;
-
+function validarRespostaNinfadoraFase1(respostaCerta, msg) {
+  var erros = 0;
   while (true) {
-    var resposta = prompt(
-      "Digite a letra da opção correta",
-      "A, B ou C?"
-    ).toLowerCase;
-    if (resposta == "c") {
-      alert("Resposta correta!");
-      location.assign("./pages/cedric-h2.html");
+    var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
+    if (resposta === respostaCerta) {
+      alert("Nossa aventura continua!");
+      location.assign("./ninfadora-fase2.html")
       break;
-    } else if (option == "a" || option == "b") {
-      if (contador >= 2) {
+    } else if (resposta === 1){
+      if (contador>=2){
+        alert("Bagman não teria apostado...");
+        location.assign('./game-over-ninfadora.html')
+        break;
+      }else{
+        alert('Hmmm... não estou sei se seguimos pelo caminho correto...')
+        location.assign('./ninfadora-fase3.html')
       }
     }
   }
