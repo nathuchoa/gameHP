9f7f087913d394cc1e45338743f4702b71cacb4f
nathuchoa <nathaliauchoa@hotmail.com>
2022-04-22 18:15:46 -0300

    incremento nas páginas: trocando o background

--- pages/cedric/cedric.js
+++ pages/cedric/cedric.js
@@ -1,20 +1,87 @@
-function validarRespostaNinfadoraFase1(respostaCerta, msg) {
-  var erros = 0;
+//FASE 1//
+
+function validarRespostaCedrictFase1(respostaCerta) {
   while (true) {
     var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
+
     if (resposta === respostaCerta) {
       alert("Nossa aventura continua!");
-      location.assign("ninfadora-fase2.html");
+      location.assign("./cedric-fase2.html");
       break;
-    } else if (resposta === 1) {
-      if (contador >= 2) {
-        alert("Bagman não teria apostado...");
-        location.assign("game-over-ninfadora.html");
-        break;
-      } else {
-        alert("Hmmm... não estou sei se seguimos pelo caminho correto...");
-        location.assign("ninfadora-fase3.html");
-      }
+    } else if (resposta === "2") {
+      alert("Bagman não teria apostado...");
+      location.assign("./game-over-cedric.html");
+      break;
+    } else (resposta === "1"); {
+      alert("Hmmm... não estou sei se seguimos pelo caminho correto...");
+      location.assign("./cedric-fase-extra.html");
+      break;
+    }
+  }
+}
+
+//FASE 2//
+
+function validarRespostaCedricFase2(respostaCerta) {
+  while (true) {
+    var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
+
+    if (resposta === respostaCerta) {
+      alert("Nossa aventura continua!");
+      location.assign("./cedric-fase3.html");
+      break;
+    } else if (resposta === "1") {
+      alert("Bagman não teria apostado...");
+      location.assign("./game-over-cedric.html");
+      break;
+    } else (resposta === "3"); {
+      alert("Hmmm... não deve ser isso. Tente outra vez!");
+      location.assign("./cedric-fase2.html");
+      break;
+    }
+  }
+}
+
+//FASE 3 - FINAL//
+
+function validarRespostacedricFase3(respostaCerta) {
+  while (true) {
+    var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
+
+    if (resposta === respostaCerta) {
+      alert("Nossa aventura continua!");
+      location.assign("../final-vencedor.html");
+      break;
+    } else if (resposta === "3") {
+      alert("Bagman não teria apostado...");
+      location.assign("game-over-cedric.html");
+      break;
+    } else (resposta === "1");{
+      alert("Não parece muito a cara do cedric! Tente outra vez.");
+      location.assign("./cedric-fase3.html");
+      break;
+    }
+  }
+}
+
+//FASE - EXTRA - CAMINHO LONGO//
+
+function validarRespostacedricFaseExtra(respostaCerta) {
+  while (true) {
+    var resposta = prompt("Digite sua resposta", "1, 2 ou 3?");
+
+    if (resposta === respostaCerta) {
+      alert("Nossa aventura continua!");
+      location.assign("./cedric-fase2.html");
+      break;
+    } else if (resposta === "1") {
+      alert("Definitivamente não estamos no caminho certo...");
+      location.assign("./game-over-cedric.html");
+      break;
+    } else if (resposta === "3"); {
+      alert("Hmmm... esse ainda não é o caminho correto. Melhor tentar do início.");
+      location.assign("./cedric-fase1.html");
+      break;
     }
   }
 }
